5deb078b5c0742d3c242b27464f56152c1cf5c5d
DCtheTall <dylancutler@google.com>
2020-08-01 17:17:35 -0400

    finish comic book shader

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import Frame from './Frame';
 import Scene from './Scene';
 import Shader from './Shader';
 import {Vector2Attribute} from './ShaderAttribute';
-import {IntegerUniform, Vector2Uniform} from './ShaderUniform';
+import {FloatUniform, IntegerUniform, Vector2Uniform} from './ShaderUniform';
 
 const FULL_VIEW_PLANE_VERTICES = [-1, 1, -1, -1, 1, 1, 1, -1];
 const FULL_PLANE_VIEW_TEX_COORDS = [1, 0, 1, 1, 0, 0, 0, 1];
+const LIGHTNESS_OFFSET = 0.05;
 
-function initScene(video: HTMLVideoElement) {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+function initScene(canvas: HTMLCanvasElement, video: HTMLVideoElement) {
   const scene = new Scene(canvas);
 
   const vertexShaderSrc = require('./shaders/vertex.glsl').default as string;
@@ -23,6 +23,8 @@ function initScene(video: HTMLVideoElement) {
     uTexture: new IntegerUniform('u_Texture', {data: 0}),
     uResolution: new Vector2Uniform(
       'u_Resolution', {data: [canvas.width, canvas.height]}),
+    uLightnessOffset: new FloatUniform(
+      'u_LightnessOffset', {data: LIGHTNESS_OFFSET}),
   });
   scene.addFrame('main', new Frame(canvas.width, canvas.height, 4, shader));
 
@@ -35,9 +37,16 @@ function initScene(video: HTMLVideoElement) {
 }
 
 document.body.onload = async function main() {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   let stream: MediaStream;
   try {
-    stream = await navigator.mediaDevices.getUserMedia({video: true});
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        width: canvas.width,
+        height: canvas.height,
+        aspectRatio: 1,
+      },
+    });
   } catch (err) {
     console.error(err);
     window.alert('Failed to get webcam video');
@@ -45,5 +54,5 @@ document.body.onload = async function main() {
   const video = document.getElementById('video') as HTMLVideoElement;
   video.srcObject = stream;
   video.play();
-  video.onplaying = () => initScene(video);
+  video.onplaying = () => initScene(canvas, video);
 }
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,11 +4,12 @@ varying vec2 v_TextureCoord;
 
 uniform sampler2D u_Texture;
 uniform vec2 u_Resolution;
+uniform float u_LightnessOffset;
 
 const mat3 BLUR_KERNEL = mat3(0.0625, 0.125, 0.0625,
                               0.125,  0.25,  0.125,
                               0.0625, 0.125, 0.0625);
-// Use the Prewitt operator for taking the gradient.
+// Use the Sobel operator for taking the gradient.
 const mat3 X_GRAD_KERNEL = mat3(1.0, 0.0, -1.0,
                                 1.0, 0.0, -1.0,
                                 1.0, 0.0, -1.0);
@@ -41,6 +42,11 @@ float intensity(vec3 color) {
   return pow(length(clamp(color, vec3(0.0), vec3(1.0))), 2.0) / 3.0;
 }
 
+// Compute the images intensity gradient using convolutions.
+// 1. First apply a 3x3 gaussian blur kernel to the texture.
+// 2. Compute the intensity of the 3x3 grid of pixels around the current coordinate.
+// 3. Apply 3x3 Sobel operator to detect edges in x and y direction.
+// Return vector
 vec2 intensityGradient(sampler2D texSampler, vec2 texCoord) {
   vec2 ds = vec2(1.0) / u_Resolution;
   mat3 M = mat3(0.0);
@@ -48,15 +54,74 @@ vec2 intensityGradient(sampler2D texSampler, vec2 texCoord) {
     for (int j = 0; j < 3; j++) {
       vec2 coord = texCoord + vec2(-ds.x + (float(i) * ds.x),
                                    -ds.y + (float(j) * ds.y));
-      M[i][j] = length(blur(texSampler, coord));
+      M[i][j] = intensity(blur(texSampler, coord));
     }
   }
   return vec2(convoluteMatrices(M, X_GRAD_KERNEL),
               convoluteMatrices(M, Y_GRAD_KERNEL));
 }
 
+// From https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+// From https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+vec3 edge(sampler2D texSampler, vec2 texCoord) {
+  vec2 result = intensityGradient(u_Texture, v_TextureCoord);
+  for (int i = 0; i < 2; i++) {
+    result[i] = pow(result[i], 0.75);
+  }
+  if (length(result) >= 0.7) {
+    result = vec2(1.0 / pow(2.0, 0.5));
+  } else if (length(result) < 0.25) {
+    result = vec2(0.2 / pow(2.0, 0.5));
+  } else if (length(result) < 0.05) {
+    result = vec2(0.0);
+  }
+  return vec3(length(result));
+}
+
+vec3 color(sampler2D texSampler, vec2 texCoord) {
+  // Convert from RGB to HSV, this makes manipulating color easier.
+  vec3 hsv = rgb2hsv(texture2D(texSampler, texCoord).xyz);
+  // Reduce the number of colors to 8.
+  float nColors = 12.0;
+  hsv[0] = floor(2.0 * nColors * hsv[0] + 0.5) / 2.0 / nColors;
+  // Add comic book dots for colors.
+  if (floor(nColors * hsv[0] + 0.5) / nColors != hsv[0]) {
+    hsv[0] = floor(nColors * hsv[0] + 0.5) / nColors;
+    if (length(texCoord - (floor(100.0 * texCoord + 0.5) / 100.0)) <= 0.002) {
+      hsv[0] -= 1.0 / nColors;
+    }
+  }
+  // Add cel shading.
+  float originalV = hsv[2];
+  float nCels = 12.0;
+  hsv[2] = floor(nCels * hsv[2] + 0.5) / nCels;
+  hsv[2] = clamp(hsv[2] + u_LightnessOffset, 0.0, 1.0);
+  // Add black dots for the darkest areas.
+  if (originalV < 0.2
+      && length(texCoord - (floor(100.0 * texCoord + 0.5) / 100.0)) <= 0.002) {
+    hsv[2] = 0.0;
+  }
+  return hsv2rgb(hsv);
+}
+
 void main() {
-  vec2 grad = intensityGradient(u_Texture, v_TextureCoord);
-  float g = pow(length(grad), 2.0);
-  gl_FragColor = vec4(1.0 - vec3(g), 1.0);
+  vec3 c = color(u_Texture, v_TextureCoord);
+  vec3 e = edge(u_Texture, v_TextureCoord);
+  gl_FragColor = vec4(c - e, 1.0);
 }
